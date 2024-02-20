88aa46ed9ac85b26cc5a540faa3c39873eea95d2
Ava Venturino <ava.venturino@mths.ca>
2024-02-20 14:16:38 +0000

    added stuff

--- Unit1-01/index.ts
+++ Unit1-01/index.ts
@@ -6,16 +6,16 @@
 * Version: 1.0
 * Since:   2020-01-01
 */
-    import { createPrompt } from 'bun-promptx'
+import { createPrompt } from 'bun-promptx'
+    
+console.log("Choose the length of the logs that you would like to move with a truck")
 
-    console.log("Choose the length of the logs that you would like to move with a truck");
+const logString = createPrompt(`Enter the lengths of the logs in meters between-> 0.25, 0.5, 1.0: `)
 
-    const logString = createPrompt(`Enter the lengths of the logs in meters between-> 0.25, 0.5, 1.0: `);
-
-    const logLength = parseFloat(logString.value);
+const logLength = parseFloat(logString.value)
 	    
-    const logOnTruck = (1100 / (logLength * 20));
+const logOnTruck = (1100 / (logLength * 20))
 
-    console.log(`The truck can carry ${logOnTruck} logs that are ${logLength} meters long.`);
+console.log(`The truck can carry ${logOnTruck} logs that are ${logLength} meters long.`)
 
-    console.log('\nDone.');
+console.log('\nDone.')
